e3e8b65767d70843507843515b2d41df4a43cedf
zdyu <1877682825@qq.com>
2022-07-18 07:46:23 +0800

    chrome Fildder extention

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Chrome.ChromeDriverExtensions;
+using OpenQA.Selenium.Chrome.Fiddler;
+using RestSharp;
+
+var driverBinary = @"D:\soft\chrome\chrome2\Chrome-bin\";
+
+ChromeOptions options = new ChromeOptions
+{
+    BinaryLocation = Path.Combine(driverBinary, "chrome.exe")
+};
+
+Environment.SetEnvironmentVariable("webdriver.chrome.driver", driverBinary);
+options.AddArgument("--disable-blink-features=AutomationControlled");
+options.AddArguments("--disable-infobars");
+List<string> ls = new List<string> { "enable-automation" };
+options.AddExcludedArguments(ls);
+
+#region 代理
+
+// var pp = GetProxy().Split(":");
+// options.AddHttpProxy(pp[0], int.Parse(pp[1]), "", "");
+
+#endregion
+
+#region Fillder
+
+options.AddFiddler(new FiddlerOption
+{
+    OnBeforeRequestOptions = new List<FiddlerOnBeforeRequestOptions>
+    {
+        new FiddlerOnBeforeRequestOptions
+        {
+            Match = "https://www.cnblogs.com/yudongdong/ajax/GetPostStat",
+            RedirectUrl = "http://localhost:5000/GetPostStat",
+            Cancel = false
+        }
+    }
+});
+
+#endregion
+
+var chrome = new ChromeDriver(driverBinary, options);
+
+
+#region method
+
+string GetProxy()
+{
+    var client = new RestClient("http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=&city=0&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=");
+    var request = new RestRequest();
+    var response = client.Execute(request);
+    return response.Content;
+}
+
+#endregion
---
+++ OpenQA.Selenium.Chrome.Fiddler/ChromeOptionsExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace OpenQA.Selenium.Chrome.Fiddler
+{
+    /// <summary>
+    /// Extensions for ChromeOptions (Selenium Driver)
+    /// 支持配置拦截指定请求(正则)
+    ///		donothing
+    ///		拦截后上传(http)
+    /// </summary>
+    public static class ChromeOptionsExtensions
+    {
+        private const string background_js = @"
+
+
+var before_configs = {before_configs};
+
+    chrome.webRequest.onBeforeRequest.addListener(
+            function(details) {
+                for (let i = 0; i<before_configs.length; i++) {
+                    var config = before_configs[i];
+                    if (!details.url.match(config.Match)){
+                        continue;
+                    }
+
+                    if (config.Cancel)
+                    {
+                        return { cancel: true};
+                    }
+                    return { redirectUrl: config.RedirectUrl};
+                }
+            },
+            { urls: ['<all_urls>']},
+            ['blocking', 'extraHeaders', 'requestBody']
+        );
+";
+
+        private const string manifest_json = @"
+{
+    ""version"": ""1.0.0"",
+    ""manifest_version"": 2,
+    ""name"": ""Chrome Fiddler"",
+    ""permissions"": [
+        ""proxy"",
+        ""tabs"",
+        ""unlimitedStorage"",
+        ""storage"",
+        ""<all_urls>"",
+        ""webRequest"",
+        ""webRequestBlocking""
+    ],
+    ""background"": {
+        ""scripts"": [""background.js""]
+	},
+    ""minimum_chrome_version"":""22.0.0""
+}";
+
+        /// <summary>
+        /// Add Fiddler extention
+        /// </summary>
+        /// <param name="options">Chrome options</param>
+        /// <param name="fiddlerOption">Proxy host</param>
+        public static void AddFiddler(this ChromeOptions options, FiddlerOption fiddlerOption)
+        {
+            var backgroundProxyJs = ReplaceTemplates(background_js, fiddlerOption);
+
+            if (!Directory.Exists("Plugins"))
+                Directory.CreateDirectory("Plugins");
+
+            var guid = Guid.NewGuid().ToString();
+
+            var manifestPath = $"Plugins/manifest_{guid}.json";
+            var backgroundPath = $"Plugins/background_{guid}.js";
+            var archiveFilePath = $"Plugins/proxy_auth_plugin_{guid}.zip";
+
+            File.WriteAllText(manifestPath, manifest_json);
+            File.WriteAllText(backgroundPath, backgroundProxyJs);
+
+            using (var zip = ZipFile.Open(archiveFilePath, ZipArchiveMode.Create))
+            {
+                zip.CreateEntryFromFile(manifestPath, "manifest.json");
+                zip.CreateEntryFromFile(backgroundPath, "background.js");
+            }
+
+            File.Delete(manifestPath);
+            File.Delete(backgroundPath);
+
+            options.AddExtension(archiveFilePath);
+        }
+
+        private static string ReplaceTemplates(string str, FiddlerOption fiddlerOption)
+        {
+            if (fiddlerOption.OnBeforeRequestOptions != null)
+            {
+                var beforeConfigs = Newtonsoft.Json.JsonConvert.SerializeObject(fiddlerOption.OnBeforeRequestOptions);
+                str = str.Replace("{before_configs}", beforeConfigs);
+            }
+            return str;
+        }
+
+
+    }
+
+    public sealed class FiddlerOption
+    {
+        public List<FiddlerOnBeforeRequestOptions>? OnBeforeRequestOptions { get; set; }
+    }
+
+    public sealed class FiddlerOnBeforeRequestOptions
+    {
+        public string Match { get; set; } = "";
+        public bool Cancel { get; set; }
+        public string RedirectUrl { get; set; } = "";
+    }
+}
